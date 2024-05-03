578dc2d9042ed9dfd159c4dd2700e9b3f4a1dd89
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-04 00:08:20 +0200

    :tada: links  navbar   :tada:

--- app/components/Home/Navigation/Nav.tsx
+++ app/components/Home/Navigation/Nav.tsx
@@ -1,8 +1,37 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='h-[12vh] bg-[#62269d] shadow-md'>
+      <div className='w-[85%] flex items-center justify-between mx-auto h-[12vh]'>
+        {/* Logo */}
+        <h1 className='text-[16px] md:text-[25px] font-bold text-white'>
+          <span className='text-[27px] md:text-[40px] text-yellow-300'>
+            IN
+          </span>
+          Vestor
+        </h1>
+        {/* Nav Links */}
+        <ul className='hidden lg:flex items-center space-x-10'>
+          <li className='text-[17px] font-semibold cursor-pointer text-yellow-300'>
+            <Link href="/">Home</Link>
+          </li>
+          <li className='text-[17px] font-semibold cursor-pointer text-white hover:text-yellow-400'>
+            <Link href="/About">About</Link>
+          </li>
+          <li className='text-[17px] font-semibold cursor-pointer text-white hover:text-yellow-400'>
+            <Link href="/Investment">Investment</Link>
+          </li>
+          <li className='text-[17px] font-semibold cursor-pointer text-white hover:text-yellow-400'>
+            <Link href="/Services">Services</Link>
+          </li>
+          <li className='text-[17px] font-semibold cursor-pointer text-white hover:text-yellow-400'>
+            <Link href="/Contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
