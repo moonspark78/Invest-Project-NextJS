038f00ef3a848488c8415d4b14a39ef4b1e17a6b
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-04 00:22:30 +0200

    :tada: working  on  navbar button   :tada:

--- app/components/Home/Navigation/Nav.tsx
+++ app/components/Home/Navigation/Nav.tsx
@@ -34,7 +34,12 @@ const Nav = () => {
         <div className='flex items-center space-x-4'>
           <button className='rounded px-10 py-2.5 overflow-hidden group bg-yellow-500 hover:bg-gradient-to-r
            hover:from-yellow-500 hover:to-yellow-400 relative text-white hover:ring-2 hover:right-offset-2
-           hover:ring-yellow-400 transition-all ease-out duration-300'></button>
+           hover:ring-yellow-400 transition-all ease-out duration-300'>
+            <span className='absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12
+            bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease'>
+            </span>
+            <span className='relative'>Login</span>
+           </button>
         </div>
       </div>
     </div>
