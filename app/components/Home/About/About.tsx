bab3c145d1f9db3d16712f9520af0f07901f3cf1
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-05 20:57:47 +0200

    :tada: done with  the about  section  :tada:

--- app/components/Home/About/About.tsx
+++ app/components/Home/About/About.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import SectionHeading from '../../Helper/SectionHeading'
 import { BsCheck2Square } from 'react-icons/bs'
+import { BiMoney } from 'react-icons/bi'
+import { GiSecurityGate } from 'react-icons/gi'
+import AboutImage from "@/public/images/about.png"
+import Image from 'next/image'
 
 const About = () => {
   return (
@@ -21,15 +25,44 @@ const About = () => {
             {/* icon list item */}
             <div className='flex mt-8 items-center space-x-4'>
                 {/* icon box */}
-                <div className=''>
-                    <BsCheck2Square className=''/>
+                <div className='w-12 h-12 rounded-full bg-pink-700 flex items-center flex-col justify-center'>
+                    <BsCheck2Square className='w-[1.3rem] h-[1.3rem] text-white'/>
                 </div>
                 {/* text box */}
-                <div></div>
+                <div>
+                  <h1 className='text-[16px] md:text-[20px] text-white font-semibold'>Licensed & Certified</h1>
+                  <p className='mt-1 text-[15px] text-white text-opacity-70'>Lorem ipsum dolor sit amet. Lorem, ipsum.</p>
+                </div>
+            </div>
+            {/* icon list item 2 */}
+            <div className='flex mt-8 items-center space-x-4'>
+                {/* icon box */}
+                <div className='w-12 h-12 rounded-full bg-blue-700 flex items-center flex-col justify-center'>
+                    <BiMoney className='w-[1.3rem] h-[1.3rem] text-white'/>
+                </div>
+                {/* text box */}
+                <div>
+                  <h1 className='text-[16px] md:text-[20px] text-white font-semibold'>Savings & Investment</h1>
+                  <p className='mt-1 text-[15px] text-white text-opacity-70'>Lorem ipsum dolor sit amet. Lorem, ipsum.</p>
+                </div>
+            </div>
+            {/* icon list item 3 */}
+            <div className='flex mt-8 items-center space-x-4'>
+                {/* icon box */}
+                <div className='w-12 h-12 rounded-full bg-orange-700 flex items-center flex-col justify-center'>
+                    <GiSecurityGate className='w-[1.3rem] h-[1.3rem] text-white'/>
+                </div>
+                {/* text box */}
+                <div>
+                  <h1 className='text-[16px] md:text-[20px] text-white font-semibold'>100% Secure Investment</h1>
+                  <p className='mt-1 text-[15px] text-white text-opacity-70'>Lorem ipsum dolor sit amet. Lorem, ipsum.</p>
+                </div>
             </div>
           </div>
           {/* Image content */}
-          <div></div>
+          <div>
+            <Image src={AboutImage} alt='about-image'/>
+          </div>
         </div>
     </div>
   )
