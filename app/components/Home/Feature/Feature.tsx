80a11d102b7a7a47eb890d0eda99c68a40becc1e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-05 21:02:24 +0200

    :tada: create  the features  section  :tada:

---
+++ app/components/Home/Feature/Feature.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SectionHeading from '../../Helper/SectionHeading'
+
+const Feature = () => {
+  return (
+    <div className='pt-20 pb-16 bg-[#032055]'>
+        {/* Section Heading */}
+        <SectionHeading heading='Why Choose Us'/>
+        {/* Define the Grid System */}
+    </div>
+  )
+}
+
+export default Feature
--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero'
 import About from './About/About'
+import Feature from './Feature/Feature'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <About/>
+      <Feature/>
     </div>
   )
 }
