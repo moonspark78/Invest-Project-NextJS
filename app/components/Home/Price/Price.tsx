8b607d5e19a7ff0ea33a04081ad72d856ce98bcf
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-06 12:28:30 +0200

    :tada: done with  the Pricing  section  :tada:

--- app/components/Home/Price/Price.tsx
+++ app/components/Home/Price/Price.tsx
@@ -7,12 +7,32 @@ const Price = () => {
     <div className='pt-20 pb-16 bg-[#5507e1]'>
         <SectionHeading heading='Our Pricing'/>
         {/* Define grid system */}
-        <div className='w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-15 mt-16'>
-            {/* Pricing Card */}
+        <div className='w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-16'>
+            {/* Pricing Card 1*/}
             <div>
                 <PriceCard
                     bg="bg-teal-700"
                     days="20"
+                    percentage="100"
+                    minInvest="100"
+                    maxInvest="1000"
+                />
+            </div>
+            {/* Pricing Card 2 */}
+            <div>
+                <PriceCard
+                    bg="bg-red-900"
+                    days="30"
+                    percentage="100"
+                    minInvest="100"
+                    maxInvest="1000"
+                />
+            </div>
+            {/* Pricing Card 3*/}
+            <div>
+                <PriceCard
+                    bg="bg-green-700"
+                    days="50"
                     percentage="100"
                     minInvest="100"
                     maxInvest="1000"
