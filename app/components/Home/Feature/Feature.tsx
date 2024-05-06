f5d95095d89caf91a2002866c5b5d7c7561fd1de
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-06 04:43:27 +0200

    :tada: working  the feature  section  :tada:

--- app/components/Home/Feature/Feature.tsx
+++ app/components/Home/Feature/Feature.tsx
@@ -14,32 +14,32 @@ const Feature = () => {
             {/* 1st feature card */}
             <div>
               <div className='p-6 cursor-pointer hover:scale-110 transition-all duration-100 bg-orange-600 rounded-md shadow-md'>
-                <FaMoneyBill className='mx-auto mt-8 w-16 h-16 text-white'/>
-                <h1 className='text-[15px] text-center text-white mt-8 mb-4 font-semibold'>Fast Profit</h1>
+                <FaMoneyBill className='mx-auto mt-8 w-14 h-14 text-white'/>
+                <h1 className='text-[15px] text-center text-white mt-4 mb-4 font-semibold'>Fast Profit</h1>
                 <p className='text-[12px] text-white text-center text-opacity-70'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, consequuntur.</p>
               </div>
             </div>
             {/* 2st feature card */}
             <div>
               <div className='p-6 cursor-pointer hover:scale-110 transition-all duration-100 bg-red-600 rounded-md shadow-md'>
-                <FaPiggyBank className='mx-auto mt-8 w-16 h-16 text-white'/>
-                <h1 className='text-[15px] text-center text-white mt-8 mb-4 font-semibold'>Instant Withdraw</h1>
+                <FaPiggyBank className='mx-auto mt-8 w-14 h-14 text-white'/>
+                <h1 className='text-[15px] text-center text-white mt-4 mb-4 font-semibold'>Instant Withdraw</h1>
                 <p className='text-[12px] text-white text-center text-opacity-70'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, consequuntur.</p>
               </div>
             </div>
             {/* 3st feature card */}
             <div>
               <div className='p-6 cursor-pointer hover:scale-110 transition-all duration-100 bg-green-600 rounded-md shadow-md'>
-                <FaMoneyBillTransfer className='mx-auto mt-8 w-16 h-16 text-white'/>
-                <h1 className='text-[15px] text-center text-white mt-8 mb-4 font-semibold'>Multi-Currency</h1>
+                <FaMoneyBillTransfer className='mx-auto mt-8 w-14 h-14 text-white'/>
+                <h1 className='text-[15px] text-center text-white mt-4 mb-4 font-semibold'>Multi-Currency</h1>
                 <p className='text-[12px] text-white text-center text-opacity-70'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, consequuntur.</p>
               </div>
             </div>
             {/* 4st feature card */}
             <div>
               <div className='p-6 cursor-pointer hover:scale-110 transition-all duration-100 bg-indigo-900 rounded-md shadow-md'>
-                <BiSupport className='mx-auto mt-8 w-16 h-16 text-white'/>
-                <h1 className='text-[15px] text-center text-white mt-8 mb-4 font-semibold'>Customer Support</h1>
+                <BiSupport className='mx-auto mt-8 w-14 h-14 text-white'/>
+                <h1 className='text-[15px] text-center text-white mt-4 mb-4 font-semibold'>Customer Support</h1>
                 <p className='text-[12px] text-white text-center text-opacity-70'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, consequuntur.</p>
               </div>
             </div>
