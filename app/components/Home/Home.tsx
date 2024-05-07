d5e415701ee8ea81574135aa202ca8954e852fee
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-07 18:49:54 +0200

    :tada: create  with  the Investor  section  :tada:

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -4,6 +4,7 @@ import About from './About/About'
 import Feature from './Feature/Feature'
 import Price from './Price/Price'
 import Advantage from './Advantage/Advantage'
+import Investor from './Investor/Investor'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Feature/>
       <Price/>
       <Advantage/>
+      <Investor/>
     </div>
   )
 }
---
+++ app/components/Home/Investor/Investor.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SectionHeading from '../../Helper/SectionHeading'
+
+const Investor = () => {
+  return (
+    <div className='pt-20 pb-16 bg-[#032055]'>
+        {/* Section Heading */}
+        <SectionHeading heading='Our Top Investors'/>
+        {/* Define the grid systeme */}
+    </div>
+  )
+}
+
+export default Investor
