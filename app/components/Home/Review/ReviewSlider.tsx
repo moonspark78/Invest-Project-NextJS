1dadccbc9151c97a01c68f73017357eaaab6d1ed
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-08 16:57:45 +0200

    :tada: working on  the ReviewSlider components  :tada:

--- app/components/Home/Review/ReviewSlider.tsx
+++ app/components/Home/Review/ReviewSlider.tsx
@@ -1,6 +1,31 @@
+
 import React from 'react'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 
 const ReviewSlider = () => {
+
+
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3,
+          slidesToSlide: 1 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 764 },
+          items: 2,
+          slidesToSlide: 1 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 764, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+    };
+
+
   return (
     <div>ReviewSlider</div>
   )
