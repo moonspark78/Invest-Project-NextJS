6000c45a1efe86fc7476f09979968cd7f2a9d806
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-09 15:22:53 +0200

    :tada: working on  the ReviewSlider component  :tada:

--- app/components/Home/Review/ReviewCard.tsx
+++ app/components/Home/Review/ReviewCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 import { FaStar } from 'react-icons/fa';
 
@@ -9,7 +10,7 @@ type Props={
 
 const ReviewCard = ({name,image}:Props) => {
   return (
-    <div className='p-6 bg-[#032055]'>
+    <div className='p-6 m-4 bg-[#032055]'>
         {/* review star */}
         <div className='flex items-center space-x-2 mt-4'>
             <FaStar className='w-4 h-4 text-yellow-500'/>
@@ -23,6 +24,18 @@ const ReviewCard = ({name,image}:Props) => {
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
             Temporibus exercitationem vero explicabo nemo neque reprehenderit.
         </p>
+        {/* Profile */}
+        <div className='flex mt-8 items-center space-x-4'>
+            <div>
+                <Image src={image} alt={name} width={60} height={60}/>
+            </div>
+            <div>
+                <h1 className='text-lg text-white uppercase'>{name}</h1>
+                <p className='mt-[0.3rem] text-[15px] text-white text-opacity-70'>
+                    CEO, WDM
+                </p>
+            </div>
+        </div>
     </div>
   )
 }
