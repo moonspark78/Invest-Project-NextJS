2262a583fb402127f4787b02d6a6c243d755bdb3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-10 17:28:02 +0200

    :tada: Done Done  :tada:

--- app/components/Home/Home.tsx
+++ app/components/Home/Home.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect } from 'react'
 import Hero from './Hero/Hero'
 import About from './About/About'
 import Feature from './Feature/Feature'
@@ -7,8 +9,27 @@ import Advantage from './Advantage/Advantage'
 import Investor from './Investor/Investor'
 import Review from './Review/Review'
 import Blog from './Blog/Blog'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+
+  useEffect(() =>{
+    const initAos = async () =>{
+      await import("aos");
+      AOS.init({
+        duration: 1000,
+        easing:"ease",
+        once: true,
+        anchorPlacement:"top-bottom"
+      });
+    }
+    initAos();
+  },[])
+
+
+
+
   return (
     <div>
       <Hero/>
