8b28fde75954112fb28616ed9ed83d2a43c6c212
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-10 04:10:28 +0200

    :tada: first the BlogCard   :tada:

--- app/components/Home/Blog/BlogCard.tsx
+++ app/components/Home/Blog/BlogCard.tsx
@@ -18,6 +18,20 @@ const BlogCard = ({image, title}:Props) => {
         height={400}
         className='object-cover rounded-lg'
         />
+        <div className='flex mt-6 items-center space-x-4'>
+            <span className='text-[15px] text-white text-opacity-60'>
+                10 may 2024
+            </span>
+            <span className='text-[15px] text-white text-opacity-60'>
+                By Admin
+            </span>
+        </div>
+        <h1 className='text-white underline cursor-pointer text-md mt-4 font-semibold'>
+            {title}
+        </h1>
+        <p className='mt-2 text-white underline cursor-pointer text-md'>
+            Read More
+        </p>
     </div>
   )
 }
